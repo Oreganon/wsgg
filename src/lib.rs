72421c686ba4c7cd03600a4b4db5f0edea800e13
Oreganon <oreganon@protonmail.com>
2023-03-04 10:26:17 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::net::TcpStream;
+use tungstenite::client::IntoClientRequest;
+use tungstenite::http::HeaderValue;
+use tungstenite::stream::MaybeTlsStream;
+use tungstenite::{connect, Message, WebSocket};
+use url::Url;
+
+struct Connection {
+    socket: WebSocket<MaybeTlsStream<TcpStream>>,
+}
+
+impl Connection {
+    pub fn new(cookie: &str) -> Result<Connection, &'static str> {
+        let endpoint = "wss://chat.strims.gg/ws";
+        let url = Url::parse(endpoint).expect("Could not parse url");
+        let mut request = url.into_client_request().expect("could not build request");
+        request.headers_mut().insert(
+            "Cookie",
+            HeaderValue::from_str(cookie).expect("Could not build cookie value"),
+        );
+        let (socket, _) = connect(request).unwrap();
+        Ok(Connection { socket })
+    }
+
+    pub fn send(mut self, message: &str) -> Result<(), &'static str> {
+        let msg = format!("MSG {{\"data\":\"{message}\"}}");
+        self.socket.write_message(Message::Text(msg)).unwrap();
+        Ok(())
+    }
+}
